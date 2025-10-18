c4c4870403ed30c3518444041cb8991983a1a30d
Phat117 <ntphat110705@gmail.com>
2025-10-18 23:33:24 +0700

    Adding Validations To CreatePostDto

--- src/posts/dtos/create-posts.dto.ts
+++ src/posts/dtos/create-posts.dto.ts
@@ -1,15 +1,63 @@
+import {
+  IsArray,
+  IsEnum,
+  IsISO8601,
+  IsJSON,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  IsUrl,
+  Matches,
+  MinLength,
+} from 'class-validator';
 import { PostStatus } from '../enums/postStatus.enum';
 import { PostType } from '../enums/postType.enum';
 
 export class CreatePostDto {
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(4)
   title: string;
+
+  @IsEnum(PostType)
+  @IsNotEmpty()
   postType: PostType;
+
+  @IsString()
+  @IsNotEmpty()
+  @Matches(/^[a-z0-9]+(?:-[a-z0-9]+)*$/, {
+    message:
+      'slug must be a string that can only contain lowercase letters, numbers, and hyphens. For example my-url',
+  })
   slug: string;
+
+  @IsEnum(PostStatus)
+  @IsNotEmpty()
   status: PostStatus;
+
+  @IsString()
+  @IsOptional()
   content?: string;
+
+  @IsOptional()
+  @IsJSON()
   schema?: string;
+
+  @IsOptional()
+  @IsUrl()
   featuredImageUrl?: string;
-  publishOn: Date;
-  tags: string[];
+
+  @IsISO8601()
+  @IsOptional()
+  publishOn?: Date;
+
+  @IsOptional()
+  @IsArray()
+  @IsString({ each: true })
+  @MinLength(3, { each: true })
+  tags?: string[];
+
+  @IsOptional()
+  @IsJSON()
   metaOptions: [{ key: 'some-key'; value: 'some-value' }];
 }

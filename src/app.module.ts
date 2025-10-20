343f9bf82db40296d5186f67bd8297455d8bed22
Phat117 <ntphat110705@gmail.com>
2025-10-20 22:08:20 +0700

    Connecting NestJS to PostgreSQL

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,25 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UsersModule } from './users/users.module';
-import { PostsModule } from './posts/posts.module';
 import { AuthModule } from './auth/auth.module';
-
+import { PostsModule } from './posts/posts.module';
+import { UsersModule } from './users/users.module';
 @Module({
-  imports: [UsersModule, PostsModule, AuthModule],
+  imports: [
+    UsersModule,
+    PostsModule,
+    AuthModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      entities: [],
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'Phattanphat1',
+      database: 'nestjs-blog',
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
